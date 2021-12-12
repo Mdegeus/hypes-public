d46bcc7b2f8c907ccc95f239492aa358ad3c1687
mdegeus <302639117@student.rocmondriaan.nl>
2021-12-13 00:25:04 +0100

    modal scripts added, page-content script added

--- hypes.js
+++ hypes.js
@@ -5,10 +5,24 @@ function sidebarChangeState(sidebarTarget, button) {
     const sidebar = document.querySelector('#' + sidebarTarget);
 
     if (sidebar) {
+
         if (open) {
             sidebar.style.left = "0";
+
         } else {
             sidebar.style.left = "-200px";
+
+        }
+
+        if (document.querySelector('.page-content')) {
+
+            const page = document.querySelector('.page-content');
+
+            if (open) {
+                page.style.paddingLeft = "200px";
+            } else {
+                page.style.paddingLeft = "0";
+            }
         }
     }
 }
@@ -33,6 +47,66 @@ if (document.querySelector(".sidebar-toggle")) {
             console.log("click");
             localStorage.setItem("open." + targetName, !JSON.parse(localStorage.getItem("open." + targetName)));
             sidebarChangeState(targetName, button);
+        })
+
+        setTimeout(() => {
+            sidebarChangeState(targetName, null);
+        },.1)
+
+    });
+}
+
+if (document.querySelector(".modal-alert")) {
+    const alerts = document.querySelectorAll(".modal-alert");
+
+    alerts.forEach(alertModal => {
+
+        console.log("found alert Modal");
+
+        if (alertModal.getAttribute('remember')) {
+            if (localStorage.getItem('alert' + alertModal.id)) {
+                alertModal.remove();
+                console.log("remove");
+            } else {
+                localStorage.setItem('alert' + alertModal.id, true);
+                console.log("found alert Modal");
+            }
+        }
+
+    });
+}
+
+if (document.querySelector(".modal-button")) {
+
+    const buttons = document.querySelectorAll(".modal-button");
+
+    buttons.forEach(button => {
+
+        const targetName = button.getAttribute('modal-id');
+
+        button.addEventListener("click", () => {
+            if (modal = document.querySelector('#' + targetName)) {
+                const modal = document.querySelector('#' + targetName);
+                modal.style.display = "flex";
+            }
+        })
+
+        sidebarChangeState(targetName, null);
+
+    });
+}
+
+if (document.querySelector(".modal-close")) {
+
+    const buttons = document.querySelectorAll(".modal-close");
+
+    buttons.forEach(button => {
+
+        const targetName = button.getAttribute('modal-id');
+
+        button.addEventListener("click", () => {
+            const modal = document.querySelector('#' + targetName);
+            modal.style.display = "none";
         })
 
         sidebarChangeState(targetName, null);
